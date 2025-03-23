06dce16dae2492dae20f9382e5c73b6ec26b6e67
Anish Kanade <127132369+AnishKanade@users.noreply.github.com>
2025-03-23 18:36:51 -0400

    Add files via upload
    Added essential files

---
+++ config_example.py
@@ -0,0 +1,6 @@
+# Rename this file to config.py and add your credentials
+TWELVE_DATA_API_KEY = "your_api_key_here"
+AWS_ACCESS_KEY_ID = "your_aws_access_key_here"
+AWS_SECRET_ACCESS_KEY = "your_aws_secret_key_here"
+AWS_REGION = "us-east-1" #or your aws region
+S3_BUCKET_NAME = "your-s3-bucket-name"
